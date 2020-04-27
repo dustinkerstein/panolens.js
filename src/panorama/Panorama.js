2ba627ff65644a403e6e1bd173b87ce175aba59d
Dustin Kerstein <dustin.kerstein@gmail.com>
2020-04-27 10:38:43 -0400

    Null check

--- src/panorama/Panorama.js
+++ src/panorama/Panorama.js
@@ -57,7 +57,7 @@ Panorama.prototype = Object.assign( Object.create( THREE.Mesh.prototype ), {
      */
     createGeometry: function ( edgeLength ) {
 
-        if (this.options.plane) {
+        if (this.options && this.options.plane) {
            return new THREE.PlaneGeometry(1, 1);
         }
 
@@ -76,7 +76,7 @@ Panorama.prototype = Object.assign( Object.create( THREE.Mesh.prototype ), {
 
         var material;
 
-        if (this.options.plane) {
+        if (this.options && this.options.plane) {
             material = new THREE.MeshBasicMaterial( { opacity: 0, transparent: true, side: THREE.FrontSide } );
         } else {
             const { fragmentShader, vertexShader } = EquirectShader;
